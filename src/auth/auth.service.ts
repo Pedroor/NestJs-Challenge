ce264b60f76403c031196e396dba154583a6cc63
Pedro Andrade <pedro@streetbeat.com>
2023-01-03 19:44:48 -0300

    feature: starting database setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Controller, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signin() {
     return { msg: 'signin' };
   }
